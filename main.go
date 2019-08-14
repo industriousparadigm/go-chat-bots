ae33b89815a4ffa91ee9756e66d7b2faed185176
mrsalvini <dsgmcosta@gmail.com>
2019-08-14 15:31:15 +0100

    bad printGreeting implemented

--- main.go
+++ main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+type englishBot struct{}
+type spanishBot struct{}
+
 func main() {
-	fmt.Println("greeting")
+	eb := englishBot{}
+	// sb := spanishBot{}
+	printGreeting(eb)
+	// printGreeting(sb)
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting)
+// }
+
+func (englishBot) getGreeting() string {
+	// VERY custom logic for eng greeting
+	return "Hey man!"
+}
+
+func (spanishBot) getGreeting() string {
+	// VERY custom logic for spanish greeting
+	return "Hola tio!"
 }
